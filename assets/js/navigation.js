6400bed9ed15adefacbf5d5afb3186203ca21ca9
rknight8 <knight.rebecca95@gmail.com>
2021-04-30 17:17:37 +0100

    update rubygems version

---
+++ assets/js/navigation.js
@@ -0,0 +1,7 @@
+function openNav() {
+  document.getElementById("menuNavigation").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("menuNavigation").style.width = "0";
+}
